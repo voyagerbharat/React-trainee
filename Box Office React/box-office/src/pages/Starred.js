f37469a39aafa6ccba8a5f70287c7f9664b810da
VoyagerBharat <monugera1@gmail.com>
2021-09-08 21:15:26 +0530

    rendered starred show page

--- Box Office React/box-office/src/pages/Starred.js
+++ Box Office React/box-office/src/pages/Starred.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
+import ShowGrid from '../Components/show/showGrid';
+import { apiGet } from '../misc/config';
+import { useShows } from '../misc/custom-hooks';
 
 const Starred = () => {
-  return <MainPageLayout>This is starred Page</MainPageLayout>;
+  const [starred] = useShows();
+  const [Shows, SetShows] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          SetShows(results);
+          setisLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setisLoading(false);
+        });
+    } else {
+      setisLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>Data is being Loaded!</div>}
+      {error && <div>Error Occured</div>}
+      {!isLoading && !error && <div>No Starred Shows</div>}
+      {!isLoading && !error && Shows && <ShowGrid data={Shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
