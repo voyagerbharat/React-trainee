e5641cb8bdf9f6cb08887dc4d997eeeda484d12b
VoyagerBharat <monugera1@gmail.com>
2021-09-16 22:03:28 +0530

    firebase initiated

---
+++ chatApp/chat-app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsp8rB8jMGnLnjSnpNZFGK_7qjY3dBMeE',
+  authDomain: 'galaxychat-ddf61.firebaseapp.com',
+  projectId: 'galaxychat-ddf61',
+  storageBucket: 'galaxychat-ddf61.appspot.com',
+  messagingSenderId: '900284334768',
+  appId: '1:900284334768:web:9c665a6c0961e19558b175',
+};
+
+export const app = firebase.initializeApp(firebaseConfig);
