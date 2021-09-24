81fe75b5ddf89f7dd2e6d95b86a9572d78c955ea
VoyagerBharat <monugera1@gmail.com>
2021-09-24 19:28:03 +0530

    side bar responsive

--- chatApp/chat-app/src/components/dashboard/DashboardToggle.js
+++ chatApp/chat-app/src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Icon, Button, Drawer } from 'rsuite';
 import Dashboard from '.';
-import { useModalState } from '../../misc/Custom-Hooks';
+import { useMediaQuery, useModalState } from '../../misc/Custom-Hooks';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaQuery('(max-width: 992px)');
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" />
         Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
