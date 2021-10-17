521bd9208d9b08ea0d668210bf00d23cb6963a74
VoyagerBharat <monugera1@gmail.com>
2021-10-17 23:24:12 +0530

    t-11 e-1 added real time presence

--- chatApp/chat-app/src/components/dashboard/DashboardToggle.js
+++ chatApp/chat-app/src/components/dashboard/DashboardToggle.js
@@ -1,18 +1,27 @@
 import React, { useCallback } from 'react';
 import { Icon, Button, Drawer, Alert } from 'rsuite';
 import Dashboard from '.';
+import { isOfflineForDatabase } from '../../context/Profile.context';
 import { useMediaQuery, useModalState } from '../../misc/Custom-Hooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
 
   const onSignOut = useCallback(() => {
-    auth.signOut();
-    Alert.info('Signed Out', 3000);
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed Out', 3000);
 
-    close();
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 3000);
+      });
   }, [close]);
   return (
     <>
--- chatApp/chat-app/src/context/Profile.context.js
+++ chatApp/chat-app/src/context/Profile.context.js
@@ -1,15 +1,28 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
 
-const ProfileContext = createContext();
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const ProfileContext = createContext();
 export const ProfileProvider = ({ children }) => {
   const [profile, setProfile] = useState(null);
   const [isLoading, setisLoading] = useState(true);
   useEffect(() => {
     let UserRef;
+    let userStatusRef;
+
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         UserRef = database.ref(`/profiles/${authObj.uid}`);
         UserRef.on('value', snap => {
           const { name, createdAt, avatar } = snap.val();
@@ -27,14 +40,37 @@ export const ProfileProvider = ({ children }) => {
         if (UserRef) {
           UserRef.off();
         }
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+        database.ref('.info/connected').off();
         setProfile(null);
         setisLoading(false);
       }
     });
+
+    database.ref('.info/connected').on('value', snapshot => {
+      // If we're not currently connected, don't do anything.
+      if (snapshot.val() === false) {
+        return;
+      }
+
+      userStatusRef
+        .onDisconnect()
+        .set(isOfflineForDatabase)
+        .then(() => {
+          userStatusRef.set(isOnlineForDatabase);
+        });
+    });
+
     return () => {
+      database.ref('.info/connected').off();
       authUnsub();
       if (UserRef) {
         UserRef.off();
+      }
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
   }, []);
