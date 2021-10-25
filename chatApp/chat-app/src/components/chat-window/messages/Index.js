be7db70fbeac97c30be807e30b37917a0197d33d
VoyagerBharat <monugera1@gmail.com>
2021-10-25 22:48:34 +0530

    t13 e3 delete message functionality

--- chatApp/chat-app/src/components/chat-window/messages/Index.js
+++ chatApp/chat-app/src/components/chat-window/messages/Index.js
@@ -78,6 +78,39 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
 
+  const handleDelte = useCallback(
+    async msgId => {
+      // eslint-disable-next-line no-alert
+      if (!window.confirm('Delete this message?')) {
+        // eslint-disable-next-line no-useless-return
+        return;
+      }
+      const isLast = messages[messages.length - 1].id === msgId;
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+
+      if (isLast && messages.length === 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = null;
+      }
+
+      try {
+        await database.ref().update(updates);
+        Alert.info('Message have been deleted', 4000);
+      } catch (err) {
+        Alert.error(err.message, 4000);
+      }
+    },
+    [chatId, messages]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No Messages yet</li>}
@@ -88,6 +121,7 @@ const Messages = () => {
             message={msg}
             handleAdmin={handleAdmin}
             handleLike={handleLike}
+            handleDelete={handleDelte}
           />
         ))}
     </ul>
--- chatApp/chat-app/src/components/chat-window/messages/MessageItem.js
+++ chatApp/chat-app/src/components/chat-window/messages/MessageItem.js
@@ -9,7 +9,7 @@ import { auth } from '../../../misc/firebase';
 import IconBtnControl from './IconBtnControl';
 import { useHover, useMediaQuery } from '../../../misc/Custom-Hooks';
 
-const MessageItem = ({ message, handleAdmin, handleLike }) => {
+const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
@@ -64,6 +64,14 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
           onClick={() => handleLike(message.id)}
           badgeContent={likeCount}
         />
+        {isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcon}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
       </div>
       <div>
         <span className="word-breal-all">{text}</span>
